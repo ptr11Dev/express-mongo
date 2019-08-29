c0a9289c6377ed99772c769fb05e45b25918978b
id github: ptr11Dev <piotrkaczmarek.dev@gmail.com>
2019-08-29 10:31:02 +0200

    add filters

--- routes/news.js
+++ routes/news.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const router = express.Router();
+const News = require("../models/news");
 
 /* GET home page. */
 router.get("/", (req, res) => {
-  res.render("news", { title: "News" });
+  const search = req.query.search;
+
+  const allNews = News.find({ title: new RegExp(search, "i") }).sort({
+    date: -1
+  });
+
+  allNews.exec((err, data) => {
+    res.render("news", { title: "News", data, search });
+  });
 });
 
 module.exports = router;
