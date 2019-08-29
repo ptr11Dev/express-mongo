763121497dd70aa7fae9b198bfb7da7975f609af
id github: ptr11Dev <piotrkaczmarek.dev@gmail.com>
2019-08-29 12:04:17 +0200

    fix config

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   db:
-    "mongodb+srv://admin:VVq9RjNhbTN1jQF2@cluster0-flede.mongodb.net/test?retryWrites=true&w=majority",
+    "mongodb+srv://admin:9R9WUNt4CcAd9GNg@cluster0-flede.mongodb.net/test?retryWrites=true&w=majority",
   keySession: ["TWOJKLUCZ"],
   maxAgeSession: 24 * 60 * 60 * 1000
 };
