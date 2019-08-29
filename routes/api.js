bfa1d9dad372de1dab920d7807b285642afeb6c2
id github: ptr11Dev <piotrkaczmarek.dev@gmail.com>
2019-08-29 12:17:35 +0200

    add api

---
+++ routes/api.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const router = express.Router();
+const News = require("../models/news");
+const defaultSort = -1;
+
+/* GET home page. */
+router.get("/", (req, res) => {
+  const search = req.query.search;
+  let sort = req.query.sort || -1;
+
+  if (sort !== -1 || sort !== 1) {
+    sort = defaultSort;
+  }
+
+  const allNews = News.find({ title: new RegExp(search, "i") }).sort({
+    date: sort
+  });
+
+  allNews.exec((err, data) => {
+    res.json(data);
+  });
+});
+
+router.get("/:id", (req, res) => {
+  const id = req.params.id;
+
+  const findNews = News.findById(id);
+
+  findNews.exec((err, data) => {
+    res.json(data);
+  });
+});
+
+module.exports = router;
